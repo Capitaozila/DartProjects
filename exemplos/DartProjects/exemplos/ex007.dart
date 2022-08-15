a63433f15667013d75db4e92a3210e7841239cff
Luis Brasil <48132029+Capitaozila@users.noreply.github.com>
2022-08-15 15:16:53 -0300

    mais um exercicio de fixação

---
+++ exemplos/DartProjects/exemplos/ex007.dart
@@ -0,0 +1,18 @@
+void main() {
+  //declaração de variáveis
+  String nome;
+  String sobrenome;
+  int idade;
+  double peso;
+  String telefone;
+
+  //atribuição de valores
+  nome = "Marnei";
+  sobrenome = "Cardoso";
+  idade = 43;
+  peso = 80;
+  telefone = "(86)99999999";
+  print(nome);
+  print(telefone);
+  print("$nome $sobrenome \n$idade anos\n$peso kilos\ntelefone:$telefone.\n");
+}
